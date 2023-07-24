c8f8ab93a4c42a6271cb6ba54e62b8264ecbfc9b
tandukar <84461389+tandukar@users.noreply.github.com>
2023-07-24 20:15:28 +0545

    create tasks (server)

---
+++ server/src/controllers/taskController.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/Users");
+const Task = require("../models/Tasks");
+
+exports.createTask = async(req, res) => {
+    try {
+        const {
+            taskName,
+            taskOwnerId,
+            taskDescription,
+            taskStatus,
+            taskPriority,
+            taskTime,
+        } = req.body;
+
+        const user = await User.findById(taskOwnerId);
+        if (!user) return res.status(400).json({ message: "User not found!" });
+
+        const newTask = await Task({
+            taskName,
+            taskOwnerId,
+            taskDescription,
+            taskStatus,
+            taskPriority,
+            taskTime,
+            taskOwner: user.username,
+        });
+        const saveTask = await newTask.save();
+        if (!saveTask) return res.status(400).json({ message: "Task not saved!" });
+        return res
+            .status(200)
+            .json({ message: "Task Created Successfully", newTask });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
