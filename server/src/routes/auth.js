81e31fda0ae6547f0eb20a85bcbabaa0adc3e8d4
tandukar <84461389+tandukar@users.noreply.github.com>
2023-08-20 14:03:43 +0545

    created update user api

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -5,5 +5,6 @@ const authController = require("../controllers/authController");
 router.post("/register", authController.createUser);
 router.post("/login", authController.loginUser);
 router.get("/users", authController.getUser)
+router.patch("/update-user/:id", authController.updateUser)
 
 module.exports = router;
